7cfb60e2f6cb722012da56f1b7d30f48c71a4e6a
千罹 <llqggl@gmail.com>
2017-08-18 18:10:17 +0800

    style: 修改 print-node-cwd.sh 打印内容

--- shell/print-node-cwd.sh
+++ shell/print-node-cwd.sh
@@ -5,5 +5,5 @@ pids=`ps -ef | grep node | grep -v "grep" | awk '{print $2}'`
 for pid in ${pids}
 do
   cwd_path=`ls -l /proc/${pid}/cwd | awk '{print $NF}'`
-  echo "cwd_path: "${cwd_path}
+  echo ${pid}"  "${cwd_path}
 done
